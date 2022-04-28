c9a14c93753eaf20985b0b2758cceb2abf5569dd
AitorElorza <aitor.elorza.segurola@gmail.com>
2022-04-28 12:46:55 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'osagarria_ep',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
