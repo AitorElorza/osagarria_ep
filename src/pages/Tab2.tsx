9d7c9fb91bc3567c28a0620309a37103ecd0fe54
anderpm <6pander6@gmail.com>
2022-05-16 13:29:11 +0200

    hook form & balioztatzea

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,9 +1,15 @@
 import { IonButton, IonCheckbox, IonCol, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonSelect, IonSelectOption, IonSlide, IonSlides, IonTitle, IonToolbar, } from '@ionic/react';
+import { useForm } from "react-hook-form";
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+
+  const { register, formState: {errors},  handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+
   return (
-    <IonPage>
+    <IonPage className='orria'>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Kontsumo optimizazioa</IonTitle>
@@ -12,11 +18,11 @@ const Tab2: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Kontsumo optimizazioa</IonTitle>
+            <IonTitle className='titulua' size="large">Kontsumo optimizazioa</IonTitle>
           </IonToolbar>
         </IonHeader>
         
-        <form className="ion-padding">
+        <form className="ion-padding" onSubmit={handleSubmit(onSubmit)}>
 
           <IonSlides>
 
@@ -24,7 +30,8 @@ const Tab2: React.FC = () => {
 
               <IonItem>
                 <IonLabel position='floating'>Bizilekua</IonLabel>
-                <IonSelect name="a" id="a" multiple={false} cancelText="Ezeztatu" okText="Gorde">
+                {errors.Bizilekua?.type === 'required' && <p className='error'> <br/>Beharrezko eremua</p>}
+                <IonSelect multiple={false} cancelText="Ezeztatu" okText="Gorde" {...register("Bizilekua", { required: true})}>
                   <IonSelectOption value="PBK">Penintsula, Balearrak eta Kanarias</IonSelectOption>
                   <IonSelectOption value="CM">Ceuta eta Melilla</IonSelectOption>
                 </IonSelect>
@@ -32,7 +39,8 @@ const Tab2: React.FC = () => {
 
               <IonItem>
                 <IonLabel position='floating'>Elektrizitate konpania</IonLabel>
-                <IonSelect multiple={false} cancelText="Ezeztatu" okText="Gorde">
+                {errors.Elektriziztate_Konpainia?.type === 'required' && <p className='error'> <br/>Beharrezko eremua</p>}
+                <IonSelect multiple={false} cancelText="Ezeztatu" okText="Gorde" {...register("Elektriziztate_Konpainia", { required: true})}>
                   <IonSelectOption value="Endesa">Endesa</IonSelectOption>
                   <IonSelectOption value="Naturgy">Naturgy</IonSelectOption>
                   <IonSelectOption value="Repsol">Repsol</IonSelectOption>
@@ -44,8 +52,10 @@ const Tab2: React.FC = () => {
               
               <IonItem>
                 <IonLabel position="floating">Potentzia kontratatua (kW)</IonLabel>
-                <IonInput/>
-              </IonItem>
+                {errors.PotentziaKontratatua?.type === 'required' && <p className='error'> <br/>Beharrezko eremua</p>}
+                {errors.PotentziaKontratatua?.type === 'pattern' && <p className='error'> <br/>Zenbaki positibo eta osoa izan behar da</p>}
+                <IonInput {...register("PotentziaKontratatua", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
+              </IonItem>                
 
             </IonSlide>
 
@@ -69,17 +79,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Hozkailua
               </h3>
+              {(errors.Hozkailu_Potentzia?.type === 'required' || errors.Hozkailu_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.Hozkailu_Potentzia?.type === 'pattern' || errors.Hozkailu_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Hozkailu_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Hozkailu_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -87,17 +99,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Izozkailua
               </h3>
+              {(errors.Izozkailu_Potentzia?.type === 'required' || errors.Izozkailu_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.Izozkailu_Potentzia?.type === 'pattern' || errors.Izozkailu_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Izozkailu_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Izozkailu_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -105,17 +119,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Router
               </h3>
+              {(errors.Ruter_Potentzia?.type === 'required' || errors.Router_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.Ruter_Potentzia?.type === 'pattern' || errors.Router_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Ruter_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Router_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -143,17 +159,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Argiztatzea
               </h3>
+              {(errors.Argiztatzea_Potentzia?.type === 'required' || errors.Argiztatzea_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.Argiztatzea_Potentzia?.type === 'pattern' || errors.Argiztatzea_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Argiztatzea_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Argiztatzea_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -161,17 +179,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Labea
               </h3>
+              {(errors.Labea_Potentzia?.type === 'required' || errors.Labea_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.Labea_Potentzia?.type === 'pattern' || errors.Labea_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Labea_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Labea_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -179,17 +199,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Garbigailua
               </h3>
+              {(errors.Garbigailua_Potentzia?.type === 'required' || errors.Garbigailua_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.Garbigailua_Potentzia?.type === 'pattern' || errors.Garbigailua_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Garbigailua_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Garbigailua_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -197,17 +219,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Plater-ikuzgailua
               </h3>
+              {(errors.PlaterIkuzgailua_Potentzia?.type === 'required' || errors.PlaterIkuzgailua_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.PlaterIkuzgailua_Potentzia?.type === 'pattern' || errors.PlaterIkuzgailua_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("PlaterIkuzgailua_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("PlaterIkuzgailua_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -215,17 +239,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Aire girotua
               </h3>
+              {(errors.AireGirotua_Potentzia?.type === 'required' || errors.AireGirotua_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.AireGirotua_Potentzia?.type === 'pattern' || errors.AireGirotua_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("AireGirotua_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("AireGirotua_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -233,17 +259,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Ordenagailua
               </h3>
+              {(errors.Ordenagailua_Potentzia?.type === 'required' || errors.Ordenagailua_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.Ordenagailua_Potentzia?.type === 'pattern' || errors.Ordenagailua_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Ordenagailua_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Ordenagailua_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -251,17 +279,19 @@ const Tab2: React.FC = () => {
               <h3 className="tresnak">
                 Berogailua
               </h3>
+              {(errors.Berogailua_Potentzia?.type === 'required' || errors.Berogailua_Orduak?.type === 'required') && <p className='error'>Beharrezko eremua</p>}
+              {(errors.Berogailua_Potentzia?.type === 'pattern' || errors.Berogailua_Orduak?.type === 'pattern') && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
               <IonRow>
                 <IonCol col-3>
                   <IonItem>
                     <IonLabel>Potentzia:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Berogailua_Potentzia", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
                 <IonCol col-9>
                   <IonItem class="padding-left-2px">
                     <IonLabel>Orduak:</IonLabel>
-                    <IonInput/>
+                    <IonInput {...register("Berogailua_Orduak", { required: true, pattern:/^[1-9][0-9]*$/ })}/>
                   </IonItem>
                 </IonCol>
               </IonRow>
@@ -279,7 +309,9 @@ const Tab2: React.FC = () => {
 
               <IonItem>
                 <IonLabel position='floating'>Mugikor kopurua</IonLabel>
-                <IonInput placeholder="Mugikor kopurua" type="number" min="0"></IonInput>
+                {errors.Mugikor_kopurua?.type === 'required' && <p className='error'> <br/>Beharrezko eremua</p>}
+                {errors.Mugikor_kopurua?.type === 'pattern' && <p className='error'>Zenbaki positibo eta osoa izan behar da</p>}
+                <IonInput placeholder="Mugikor kopurua" type="number" min="0" {...register("Mugikor_kopurua", { required: true, pattern:/^[1-9][0-9]*$/ })}></IonInput>
               </IonItem>
 
               {/*
@@ -294,7 +326,7 @@ const Tab2: React.FC = () => {
                 <IonLabel>Terminoak irakurri eta onartzen ditut</IonLabel>
                 <IonCheckbox slot="start" />
               </IonItem>
-              <IonButton className="ion-margin-top" type="submit" expand="block">
+              <IonButton className="ion-margin-top" href="../pages/Tab1" type="submit" expand="block">
                 Kalkulatu
               </IonButton>
 
